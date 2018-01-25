92a1d1557d7fc8ab815b31afb1cc8739f120de75
LelWang <1335450580@qq.com>
2018-01-25 12:46:29 +0800

    gg

---
+++ slcj/js/script.js
@@ -0,0 +1,26 @@
+﻿
+window.onload = function(){
+	var swiper = new Swiper('.swiper-container', {
+	direction: 'vertical',
+	onInit: function(swiper) { //Swiper2.x的初始化是onFirstInit
+		swiperAnimateCache(swiper); //隐藏动画元素 
+		swiperAnimate(swiper); //初始化完成开始动画
+	},
+	onSlideChangeEnd: function(swiper) {
+		swiperAnimate(swiper); 
+	}
+});
+var bgmc=$("#bgmc").get(0);
+$(".bgmcBtn").click(function(){
+	 if(bgmc.paused){
+            bgmc.play();
+            $(".bgmcBtn").html('<img src="slcj/img/musicBtn.png"/>');
+        }else{
+            bgmc.pause();
+            $(".bgmcBtn").html('<img src="slcj/img/musicBtnOff.png"/>');
+        }
+	});
+};
+	
+
+
